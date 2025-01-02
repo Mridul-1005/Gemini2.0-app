cfa5328f7ca7fb90d056c876d240ace8da28979c
Mridul-1005 <mhdanger193@gmail.com>
2025-01-03 01:47:40 +0600

    thirdmilestone

--- gemini-clone/src/App.jsx
+++ gemini-clone/src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Sidebar from './Component/Sidebar/Sidebar'
+import Main from './Component/Main/Main'
 
 const App = () => {
   return (
     <>
       <Sidebar/>
+      <Main/>
     </>
   )
 }
