6502f34ff4b8a0459b1d0bc22e25750de7bd3b43
Mridul-1005 <mhdanger193@gmail.com>
2025-01-04 00:52:37 +0600

    secondday

--- gemini-clone/src/Component/Main/Main.jsx
+++ gemini-clone/src/Component/Main/Main.jsx
@@ -8,6 +8,43 @@ const Main = () => {
         <p>Gemini2.0</p>
         <img src={assets.user_icon} alt="" />
       </div>
+      <div className="main-container">
+        <div className="greet">
+            <p><span>Hello, bro.</span></p>
+            <p>How can I help you today</p>
+        </div>
+       <div className="cards">
+        <div className="card">
+            <p>Suggest a beautiful place to see on an upcoming road trip</p>
+            <img src={assets.compass_icon} alt="" />
+        </div>
+        <div className="card">
+            <p>Briefly summarize this concept: urban planning</p>
+            <img src={assets.bulb_icon} alt="" />
+        </div>
+        <div className="card">
+            <p>Brainstorm team bonding activities for our work retreat</p>
+            <img src={assets.message_icon} alt="" />
+        </div>
+        <div className="card">
+            <p>Improve the readability of the following code</p>
+            <img src={assets.code_icon} alt="" />
+        </div>
+        </div> 
+        <div className="main-bottom">
+            <div className="search-box">
+           <input type='text' placeholder='Enter a prompt here'/>     
+           <div>
+            <img src={assets.gallery_icon} alt="" />
+            <img src={assets.mic_icon} alt="" />
+            <img src={assets.send_icon} alt="" />
+           </div>
+            </div>
+            <p className="bottom-info">
+          Gemini2.0 may display inaccurate info, including about people, so double-check it's responses       
+            </p>
+        </div>
+      </div>
     </div>
   )
 }
