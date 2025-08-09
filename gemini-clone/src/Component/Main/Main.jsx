19202c10b9d6af44a6d833ef284f7a84ea957dd1
Mridul-1005 <mhdanger193@gmail.com>
2025-08-10 04:51:48 +0600

    added loader

--- gemini-clone/src/Component/Main/Main.jsx
+++ gemini-clone/src/Component/Main/Main.jsx
@@ -42,7 +42,23 @@ const Main = () => {
         </>
         :
         <div className='result'>
-          
+           <div className="result-title">
+            <img src={assets.user_icon} alt="" />
+            <p>{recentPrompt}</p>
+           </div>
+           <div className="result-data">
+            <img src={assets.gemini_icon} alt="" />
+            {loading?
+             <div className='loader'>
+
+              <hr />
+              <hr />
+              <hr />
+              </div>  : <p dangerouslySetInnerHTML={{__html: resultData}}></p>
+
+          }
+            
+           </div>
           </div>
 
 }
--- gemini-clone/src/context/Context.jsx
+++ gemini-clone/src/context/Context.jsx
@@ -19,12 +19,12 @@ const ContextProvider = (props) =>{
     setResultData("");
     setLoading(true);
     setShowResult(true);
-
+    setRecentPrompt(input);
     const response = await runChat(input)
     setResultData(response);
     setLoading(false);
     setInput("");
-    
+
    }
 
   
