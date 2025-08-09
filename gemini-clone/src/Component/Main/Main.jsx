2bbe64974b4eee598dcba4e50c6e54dca950664b
Mridul-1005 <mhdanger193@gmail.com>
2025-08-10 04:26:23 +0600

    added gemini api

--- gemini-clone/src/Component/Main/Main.jsx
+++ gemini-clone/src/Component/Main/Main.jsx
@@ -1,7 +1,11 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import './Main.css'
 import { assets } from '../../assets/assets'
+import { Context } from '../../context/Context'
 const Main = () => {
+
+  const {onSent, recentPrompt, showResult, loading, resultData, setInput, input} = useContext(Context)
+
   return (
     <div className='main'>
       <div className="nav">
@@ -9,7 +13,11 @@ const Main = () => {
         <img src={assets.user_icon} alt="" />
       </div>
       <div className="main-container">
-        <div className="greet">
+
+       {!showResult?
+       
+      <>
+       <div className="greet">
             <p><span>Hello, bro.</span></p>
             <p>How can I help you today</p>
         </div>
@@ -31,13 +39,22 @@ const Main = () => {
             <img src={assets.code_icon} alt="" />
         </div>
         </div> 
+        </>
+        :
+        <div className='result'>
+          
+          </div>
+
+}
+
+       
         <div className="main-bottom">
             <div className="search-box">
-           <input type='text' placeholder='Enter a prompt here'/>     
+           <input onChange={(e)=> setInput(e.target.value)} value={input} type='text' placeholder='Enter a prompt here'/>     
            <div>
             <img src={assets.gallery_icon} alt="" />
             <img src={assets.mic_icon} alt="" />
-            <img src={assets.send_icon} alt="" />
+            <img onClick={()=>onSent()} src={assets.send_icon} alt="" />
            </div>
             </div>
             <p className="bottom-info">
